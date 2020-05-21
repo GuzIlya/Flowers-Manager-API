92cd0b1d296a1ee26d18ead39ee13d2245fa9cbb
Ilya Guz <ilya.guz@tut.by>
2020-05-21 22:34:05 +0300

    fixing error

--- src/main/java/net/guz/flowersmanagerapi/FlowersManagerApiApplication.java
+++ src/main/java/net/guz/flowersmanagerapi/FlowersManagerApiApplication.java
@@ -2,8 +2,13 @@ package net.guz.flowersmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("net.guz.flowersmanagerapi.repository")
+@EntityScan("service.models")
 public class FlowersManagerApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(FlowersManagerApiApplication.class, args);
